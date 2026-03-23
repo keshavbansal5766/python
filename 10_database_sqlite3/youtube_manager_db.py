9bbdb554e49e81e486496430b521ecb45486de0a
Keshav Bansal <keshav22.dlp@gmail.com>
2026-03-23 15:41:24 +0530

    complete sqlite project

--- 10_database_sqlite3/youtube_manager_db.py
+++ 10_database_sqlite3/youtube_manager_db.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("youtube_videos.db")
 
 cursor = conn.cursor()
 
-cursor.execute('''  
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS videos (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
@@ -12,19 +12,33 @@ cursor.execute('''
     )
 ''')
 
+
 def list_videos():
     cursor.execute("SELECT * FROM videos")
-    for row in cursor.fetchall():
-        print(row)
+    rows = cursor.fetchall()
+    if not rows:
+        print("no videos available")
+    else:
+        for row in rows:
+            print(row)
 
-def add_video():
-    pass
 
-def update_video():
-    pass
+def add_video(name, time):
+    cursor.execute(
+        "INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
+    conn.commit()
 
-def delete_video():
-    pass
+
+def update_video(video_id, new_name, new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?",
+    (new_name, new_time, video_id))
+    conn.commit()
+
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    conn.commit()
+
 
 def main():
     while True:
@@ -37,16 +51,16 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == "1":
-           list_videos()
+            list_videos()
         elif choice == "2":
-            name = input("Enter the new video name")
-            time = input("Enter the new video time")
+            name = input("Enter the new video name: ")
+            time = input("Enter the new video time: ")
             add_video(name, time)
         elif choice == "3":
-            video_id = input("Enter video ID to update: ")
-            name = input("Enter the new video name")
-            time = input("Enter the new video time")
-            update_video(name, time, video_id)
+            video_id = int(input("Enter video ID to update: "))
+            name = input("Enter the new video name: ")
+            time = input("Enter the new video time: ")
+            update_video(video_id, name, time)
         elif choice == "4":
             video_id = input("Enter video ID to delete: ")
             delete_video(video_id)
@@ -54,8 +68,10 @@ def main():
             break
         else:
             print("Invalid Choice...")
-            
+
+# house keeping from preventing database corruption
     conn.close()
+
 
 if __name__ == "__main__":
     main()
